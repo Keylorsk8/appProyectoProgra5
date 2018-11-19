e26ac1241d12b78992799f2364d1cbdcd1b55470
Pablo <Pablo@DESKTOP-PF3NVEE>
2018-11-19 11:49:53 -0600

    Pablo Rojas 19/11/2018 11:49 am
    Mantenimiento Coordinacion y el Bean de coordinacion

---
+++ src/java/controller/beanMantCoordinacion.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Pablo
+ */
+@Named(value = "beanMantCoordinacion")
+@SessionScoped
+public class beanMantCoordinacion implements Serializable {
+
+    /**
+     * Creates a new instance of beanMantCoordinacion
+     */
+    public beanMantCoordinacion() {
+    }
+    
+    public void validacion(){
+        
+    }
+    
+    public void cancelar(){
+        
+    }
+    
+}
